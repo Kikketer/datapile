55863986b109ecda28e4ce67c031ed85d3107b57
Chris Weed <christopher.weed@libertymutual.com>
2016-01-29 08:54:17 -0600

    Improved app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
  *
  * The goal is to have 0 external dependencies, NPM install is known to break within
  * our enterprise environment.  So if you have Node you can run this critter!
+ *
+ * TODO: Allow command line to adjust port
+ * TODO: Allow command line to adjust the mock-data folder location
+ * TODO: Potentially have a .mockserver config file
  */
 
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var port = 3001;
 
 http.createServer(function(req, res) {
@@ -25,30 +30,38 @@ http.createServer(function(req, res) {
       req.url += '_' + req.method + '.json';
       if (!req.url.match(/mock\-data/gi)) {
         req.url = '/mock-data' + req.url;
+        //req.url = req.url.replace(/\/mock\-data/, '');
       }
 
       // Add a slight delay
       setTimeout(function() {
-        fs.readFile(req.url, {encode: 'utf8'}, function(err, datr) {
+        fs.readFile(path.join(__dirname, req.url), {encode: 'utf8'}, function(err, datr) {
           res.writeHead(200, {'Content-Type': 'application/json'});
           res.end(datr);
         });
-      }, 1000);
+      }, 500);
     }
 
-    console.log(req.url);
+    console.log('200: ' + req.url);
   }
+  // Simply serve up a 404 if it's an invalid data endpoint (pure data server)
   else {
-    req.url = '.' + req.url;
-    if (req.url.match(/\/$/)) {
-      req.url += 'index.html';
-    }
-    console.log(req.url);
-    fs.readFile(req.url, {encoding: 'utf8'}, function(err, item) {
-      res.writeHead(200);
-      res.end(item);
-    });
+    console.log('404: ' + req.url);
+    res.writeHead(404);
+    res.end();
   }
+  // Enable this if we want to serve up static web content
+  //else {
+  //  req.url = '.' + req.url;
+  //  if (req.url.match(/\/$/)) {
+  //    req.url += 'index.html';
+  //  }
+  //  console.log(req.url);
+  //  fs.readFile(req.url, {encoding: 'utf8'}, function(err, item) {
+  //    res.writeHead(200);
+  //    res.end(item);
+  //  });
+  //}
 
 }).listen(port, 'localhost', function() {
   console.log('Server running on port ' + port);
